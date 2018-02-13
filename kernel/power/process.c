1739ee5e464a32f5cc3c9987c746d895f445179e
YaroST12 <yaro330@gmail.com>
2018-02-13 17:10:33 +0300

    kernel/power/process: adapt thaw fingerprint hack to Oreo Name changed, fingerprint/fingerprintd variants didn't work. Signed-off-by: YaroST12 <yaro330@gmail.com>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -221,6 +221,32 @@ int freeze_kernel_threads(void)
 	return error;
 }
 
+<<<<<<< HEAD
+=======
+#ifdef CONFIG_PRODUCT_Z2_PLUS
+void thaw_fingerprintd(void)
+{
+	struct task_struct *p;
+
+	pm_freezing = false;
+	pm_nosig_freezing = false;
+
+	read_lock(&tasklist_lock);
+	for_each_process(p) {
+		if (!memcmp(p->comm, "fingerprint@2.1", 16)) {
+			pr_err("fingerprint thawed\n");
+			__thaw_task(p);
+			break;
+		}
+	}
+	read_unlock(&tasklist_lock);
+
+	pm_freezing = true;
+	pm_nosig_freezing = true;
+}
+#endif
+
+>>>>>>> 963e8ded3219... kernel/power/process: adapt thaw fingerprint hack to Oreo
 void thaw_processes(void)
 {
 	struct task_struct *g, *p;
